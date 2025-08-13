ee6d3a37237c917834ac5a5393cc8ba5e71c68a7
Shakirat Akanji <akanjishakirato@gmail.com>
2025-08-13 20:51:57 +0100

    Updated Calculator and InputField components

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,11 +3,24 @@ import Button from "./Button";
 
 export default function Calculator() {
   return (
-    <section className="flex w-full flex-col p-[2.5rem] items-start gap-[2.5rem] grow">
-      <InputField>Mortgage Amount</InputField>
-      <InputField unit="years">Mortgage Term</InputField>
-      <InputField unit="%">Interest Rate</InputField>
+    <form className="flex w-full flex-col p-[2.5rem] items-start gap-[2.5rem]">
+      <div className="flex-1 flex flex-row justify-between items-center">
+        <h2>Mortgage Calculator</h2>
+        <button>Clear All</button>
+      </div>
+
+      <div className="">
+        <InputField unit="￡">Mortgage Amount</InputField>
+
+        <div className="gap-[1.5rem]">
+          <InputField unit="years">Mortgage Term</InputField>
+          <InputField unit="%">Interest Rate</InputField>
+        </div>
+
+        <div></div>
+      </div>
+
       <Button>Calculate Repayments</Button>
-    </section>
+    </form>
   );
 }
--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,15 +1,15 @@
 export default function InputField({ unit, children }) {
   return (
-    <div className="flex flex-col gap-[0.75rem] items-start self-stretch">
+    <div className="flex-1/2 flex-col gap-[0.75rem] items-start">
       <label className="text-slate-700 text-[1rem] font-medium leading-[100%]">
         {children}
       </label>
-      <div className="flex flex-row items-start self-stretch rounded-[0.25rem] border border-slate-500 h-[3rem]">
+      <div className="flex flex-row items-start rounded-[0.25rem] border border-slate-500 h-[3rem] overflow-hidden">
         <input
-          className="flex px-[1rem] items-center gap-[i.5rem] grow self-stretch text-slate-900 text-[1.125rem] font-semibold leading-[125%] border-none focus:outline-none"
+          className="flex flex-1/2 px-[1rem] items-center gap-[1.5rem] text-slate-900 text-[1.125rem] font-semibold leading-[125%] border-none focus:outline-none"
           type="text"
         />
-        <div className="flex flex-col, justify-center items-center gap-[0.5rem] self-stretch bg-slate-100 px-[1rem] py-[0.75rem] text-slate-700 text-[1.125rem] font-semibold leading-[125%]">
+        <div className="flex flex-col, justify-center items-center gap-[0.5rem] bg-slate-100 px-[1rem] py-[0.75rem] text-slate-700 text-[1.125rem] font-semibold leading-[125%]">
           {unit}
         </div>
       </div>
